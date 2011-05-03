23fa72a6d050350b7aa5d267c599d74edfaf87dc
dmitry <dmitry@8b1d8639-bf04-4f60-8689-d39f91c81475>
2011-05-03 23:04:23 +0000

    ObjectFactory Interface

---
+++ src/mettel/generator/java/MettelObjectFactoryInterfaceFile.java
@@ -0,0 +1,67 @@
+/**
+ * This file is part of MetTeL.
+ *
+ * MetTeL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MetTeL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MetTeL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package mettel.generator.java;
+
+import mettel.util.MettelJavaNames;
+
+/**
+ * @author Dmitry Tishkovsky
+ * @version $Revision$ $Date$
+ *
+ */
+public class MettelObjectFactoryInterfaceFile extends MettelJavaInterfaceFile {
+
+	private String prefix = "Mettel";
+	/**
+	 * @param name
+	 * @param pack
+	 */
+	public MettelObjectFactoryInterfaceFile(String prefix, MettelJavaPackage pack) {
+		super(prefix+"ObjectFactory", pack, null);
+		this.prefix = prefix;
+	}
+
+
+	public void addCreateMethod(String connective, String sort, String[] children, String[] cSorts){
+		final String TYPE = MettelJavaNames.firstCharToUpperCase(connective) +
+							MettelJavaNames.firstCharToUpperCase(sort);
+		final int SIZE = children.length;
+		String[] types = new String[SIZE];
+		for(int i = 0; i < SIZE; i++){
+			types[i] = MettelJavaNames.firstCharToUpperCase(children[i]) +
+					   MettelJavaNames.firstCharToUpperCase(cSorts[i]);
+		}
+		addCreateMethod(TYPE,types);
+	}
+
+
+	public void addCreateMethod(String type, String[] types){
+		final String TYPE = prefix + type;
+		final int SIZE = types.length;
+
+		indent();
+		if(SIZE > 0){
+			append(TYPE + " create" + type +"(" + prefix + types[0] +"e0");
+			for(int i = 1; i < SIZE; i++){
+				append(", " + prefix + types[i] + 'e' + i);
+			}
+			append(");");
+		}else if(SIZE == 0){
+			append(TYPE + ' ' + type + "Constant();");
+		}
+	}
+}
