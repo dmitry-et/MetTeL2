5f8112a75118d106583b296b8cc5a03cbb83699d
dmitry <dmitry@8b1d8639-bf04-4f60-8689-d39f91c81475>
2011-12-16 20:31:29 +0000

    DFLR strategy

---
+++ src/mettel/core/MettelTableauStateReverseNaturalComparator.java
@@ -0,0 +1,48 @@
+/**
+ * This file is part of MetTeL.
+ *
+ * MetTeL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MetTeL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MetTeL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package mettel.core;
+
+import java.util.Comparator;
+
+/**
+ * @author Dmitry Tishkovsky
+ * @version $Revision$ $Date$
+ *
+ */
+public class MettelTableauStateReverseNaturalComparator implements Comparator<MettelTableauState> {
+
+	/**
+	 *
+	 */
+	public MettelTableauStateReverseNaturalComparator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public int compare(MettelTableauState s1, MettelTableauState s2) {
+		if(s1 == null){
+			if(s2 == null) return 0;
+			return -1;
+		}
+		if(s2 == null) return 1;
+		return (-s1.compareTo(s2));
+	}
+
+}
