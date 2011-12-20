05605e449ffe705a51a8e84979276fe40144df73
dmitry <dmitry@8b1d8639-bf04-4f60-8689-d39f91c81475>
2011-12-20 20:09:24 +0000

    LTLC case study: initial definitions

---
+++ test/examples/LTLC/LTLC.s
@@ -0,0 +1,47 @@
+//Specification of linear temporal logic LTL with Boolean constraints
+specification LTLC;
+
+syntax LTLC{
+	sort formula;
+	sort nominal;
+
+	formula true = 'true';
+	formula false = 'false';
+
+	formula singleton = '{' nominal '}';
+
+	formula negation = '~' formula;
+	formula next = '()' formula;
+
+	formula diamond = '<>' formula;
+	formula box = '[]' formula;
+
+	formula at = '@' nominal formula;
+
+	formula conjunction  = formula '&' formula;
+	formula disjunction  = formula '|' formula;
+
+	formula implication = formula '->' formula;
+
+	formula equivalence = formula '<->' formula;
+
+	formula until = formula 'U' formula;
+	formula waitFor = formula 'W' formula;
+
+	//Tableau additions
+	formula eventualityDiamond = 'E' '(' formula ')';
+	formula eventualityU = 'EU' '(' formula ',' formula ')';
+
+	formula equality = '[' nominal '=' nominal ']';
+	nominal f = 'f' '('  nominal ')'; //Skolem function == successor function
+	formula badLoop = 'badLoop';
+
+	//Constraints
+	formula C02 = 'C02' '(' formula ',' formula ')';
+	formula C12 = 'C12' '(' formula ',' formula ')';
+	formula C22 = 'C22' '(' formula ',' formula ')';
+	formula C03 = 'C03' '(' formula ',' formula ',' formula ')';
+	formula C13 = 'C13' '(' formula ',' formula ',' formula ')';
+	formula C23 = 'C23' '(' formula ',' formula ',' formula ')';
+	formula C33 = 'C33' '(' formula ',' formula ',' formula ')'
+}
